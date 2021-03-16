b94527629c07c9c0883b943fede7031399d1a5e9
Frances Dalla Torre <francesdallatorre@MacBook-Pro.local>
2021-03-16 12:05:10 -0400

    added cors

--- server.js
+++ server.js
@@ -11,13 +11,22 @@ mongoose.connect(`mongodb://localhost:27017/${DBNAME}`, { useNewUrlParser: true 
 mongoose.connection.once('open', () => {
     console.log('connected to mongoose...');
 });
+const whitelist = ['http://localhost:3000']
+const corsOptions = {
+    origin: function (origin, callback) {
+        if (whitelist.indexOf(origin) !== -1) {
+            callback(null, true)
+        } else {
+            callback(new Error('Not allowed by CORS'))
+        }
 
-// APP.use(cors(corsOptions));
 
-APP.use(express.json());
+        APP.use(cors(corsOptions));
 
-APP.use('/cocktails', cocktailsController)
+        APP.use(express.json());
 
-APP.listen(PORT, () => {
-    console.log("listening on port: ", PORT)
-})
+        APP.use('/cocktails', cocktailsController)
+
+        APP.listen(PORT, () => {
+            console.log("listening on port: ", PORT)
+        })
