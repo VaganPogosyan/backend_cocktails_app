2f25cc9f0bc5647f0030b8ce88119d39ba68fe1a
Frances Dalla Torre <francesdallatorre@MacBook-Pro.local>
2021-03-16 14:15:52 -0400

    test

--- server.js
+++ server.js
@@ -19,14 +19,16 @@ const corsOptions = {
         } else {
             callback(new Error('Not allowed by CORS'))
         }
+    }
+}
 
 
-        APP.use(cors(corsOptions));
+APP.use(cors(corsOptions));
 
-        APP.use(express.json());
+APP.use(express.json());
 
-        APP.use('/cocktails', cocktailsController)
+APP.use('/cocktails', cocktailsController)
 
-        APP.listen(PORT, () => {
-            console.log("listening on port: ", PORT)
-        })
+APP.listen(PORT, () => {
+    console.log("listening on port: ", PORT)
+})
