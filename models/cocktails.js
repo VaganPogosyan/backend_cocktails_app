35d808f9b18a5930e45545b6363e81970ab53371
Frances Dalla Torre <francesdallatorre@MacBook-Pro.local>
2021-03-16 21:21:52 -0400

    rendering cocktails search results

--- models/cocktails.js
+++ models/cocktails.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const cocktailSchema = mongoose.Schema({
     name: { type: String },
-    image: { type: String },
+    image: { type: String }
 });
 
 module.exports = mongoose.model('Cocktail', cocktailSchema)
--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const mongoose = require('mongoose');
-const cors = require('cors');
+// const cors = require('cors');
 
 const cocktailsController = require('./controllers/cocktails');
 const APP = express();
@@ -11,19 +11,21 @@ mongoose.connect(`mongodb://localhost:27017/${DBNAME}`, { useNewUrlParser: true 
 mongoose.connection.once('open', () => {
     console.log('connected to mongoose...');
 });
-const whitelist = ['http://localhost:3000']
-const corsOptions = {
-    origin: function (origin, callback) {
-        if (whitelist.indexOf(origin) !== -1) {
-            callback(null, true)
-        } else {
-            callback(new Error('Not allowed by CORS'))
-        }
-    }
-}
 
 
-APP.use(cors(corsOptions));
+// const whitelist = ['http://localhost:3000']
+// const corsOptions = {
+//     origin: function (origin, callback) {
+//         if (whitelist.indexOf(origin) !== -1) {
+//             callback(null, true)
+//         } else {
+//             callback(new Error('Not allowed by CORS'))
+//         }
+//     }
+// }
+
+
+// APP.use(cors(corsOptions));
 
 APP.use(express.json());
 
