aa06a2abb407cf7487b1e2e0f1c3ddd299fceffb
Vagan Pogosyan <vaganpogosyan@macbook-pro.myfiosgateway.com>
2021-03-19 10:05:48 -0400

    set up server for deployment on heroku

--- server.js
+++ server.js
@@ -4,13 +4,24 @@ const cors = require('cors');
 
 const cocktailsController = require('./controllers/cocktails');
 const APP = express();
-const PORT = 3003;
+const PORT = process.env.PORT || 3003;
 const DBNAME = 'cocktails';
+const db = mongoose.connection;
 
-mongoose.connect(`mongodb://localhost:27017/${DBNAME}`, { useNewUrlParser: true });
-mongoose.connection.once('open', () => {
-    console.log('connected to mongoose...');
-});
+const MONGODB_URI = process.env.MONGODB_URI || `mongodb://localhost:27017/${DBNAME}`
+
+mongoose.connect(MONGODB_URI, { useNewUrlParser: true });
+
+// Error / success
+db.on('error', (err) => console.log(err.message + ' is Mongod not running?'));
+db.on('connected', () => console.log('mongo connected: ', MONGODB_URI));
+db.on('disconnected', () => console.log('mongo disconnected'));
+
+// mongoose.connection.once('open', () => {
+//     console.log('connected to mongoose...');
+// });
+
+db.on('open', () => { });
 
 
 const whitelist = ['http://localhost:3000']
@@ -24,7 +35,7 @@ const corsOptions = {
     }
 }
 
-APP.use(cors(corsOptions));
+// APP.use(cors(corsOptions));
 
 APP.use(cors(corsOptions));
 
